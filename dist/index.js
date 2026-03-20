fcc80e4a682d2ec393ae2be58073f9bac37576f1
Canadas7 <nicholas.camillo7@gmail.com>
2026-03-19 22:08:39 -0300

    atividade-2

--- dist/index.js
+++ dist/index.js
@@ -5,3 +5,8 @@ function listarConsultasPorStatus(
   return consultas.filter((consulta) => consulta.status === status);
 }
 
+function listarConsultasFuturas(consultas: Consulta[]): Consulta[] {
+  const hoje = new Date();
+  hoje.setHours(0, 0, 0, 0); // Zera horas para comparar apenas a data
+  return consultas.filter((consulta) => consulta.data >= hoje);
+}
